7a3a34c3b8900431c25c2c6fc5aaec9d272a6e7c
Amit Mundra <amit.mundra52@gmail.com>
2018-05-01 14:36:52 +0530

    initial rendering component which is the website page

---
+++ src/app/website/website.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'website',
+  templateUrl: './website.component.html',
+  styleUrls: ['./website.component.css']
+})
+export class Website {
+  title = 'app';
+}
