4a4fe367d990dfaf01878ea8b1a12d68b0ec0cd7
Amit Mundra <amit.mundra52@gmail.com>
2018-05-03 13:18:01 +0530

    Header background changes with responsive

--- src/app/website.directives/website.directives.header/website.directives.header.ts
+++ src/app/website.directives/website.directives.header/website.directives.header.ts
@@ -10,8 +10,4 @@ export class WebsiteHeader {
     constructor() {
         this.navItemList = [{name: 'About Us', class: 'nav-link active'},{name: 'Admissions', class: 'nav-link active'},{name: 'Academics', class: 'nav-link active'},{name: 'Blog', class: 'nav-link active'},{name: 'Research', class: 'nav-link active'},{name: 'Contact Us', class: 'nav-link active'}];
     }
-    changeBackGround () {
-        document.getElementById('header').style.backgroundImage = '../../../brand.png';
-    }
-    
 }
